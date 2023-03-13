568ad5817f8ee02b828d7be33cdbd2958522faee
czc <1060094938@qq.com>
2023-03-14 00:45:29 +0800

    admin

---
+++ rural-mbg/src/main/java/cn/czc/rural/mapper/UmsResourceCategoryMapper.java
@@ -0,0 +1,30 @@
+package cn.czc.rural.mapper;
+
+import cn.czc.rural.model.UmsResourceCategory;
+import cn.czc.rural.model.UmsResourceCategoryExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UmsResourceCategoryMapper {
+    long countByExample(UmsResourceCategoryExample example);
+
+    int deleteByExample(UmsResourceCategoryExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UmsResourceCategory row);
+
+    int insertSelective(UmsResourceCategory row);
+
+    List<UmsResourceCategory> selectByExample(UmsResourceCategoryExample example);
+
+    UmsResourceCategory selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("row") UmsResourceCategory row, @Param("example") UmsResourceCategoryExample example);
+
+    int updateByExample(@Param("row") UmsResourceCategory row, @Param("example") UmsResourceCategoryExample example);
+
+    int updateByPrimaryKeySelective(UmsResourceCategory row);
+
+    int updateByPrimaryKey(UmsResourceCategory row);
+}
